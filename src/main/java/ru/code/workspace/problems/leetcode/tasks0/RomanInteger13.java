c89e930aeeb546dd2a0f806d46eefabea6c12cc8
krylov-sergey-sergeevich <krylov.sergey.sergeevich@gmail.com>
2024-11-02 19:59:39 +0300

    13. Roman to Integer

--- src/main/java/ru/code/workspace/problems/leetcode/tasks0/RomanInteger13.java
+++ src/main/java/ru/code/workspace/problems/leetcode/tasks0/RomanInteger13.java
@@ -23,16 +23,56 @@ public class RomanInteger13 {
         map.put(String.valueOf('M'), 1000);
         map.put("IV", 4);
         while(idx < s.length()) {
+            if (s.substring(idx).startsWith("CD")) {
+                result += 400;
+                idx +=2;
+            } else
+            if (s.substring(idx).startsWith("CM")) {
+                result += 900;
+                idx +=2;
+            } else
+            if (s.substring(idx).startsWith("XL")) {
+                result += 40;
+                idx +=2;
+            } else
+            if (s.substring(idx).startsWith("XC")) {
+                result += 90;
+                idx +=2;
+            } else
+            if (s.substring(idx).startsWith("IV")) {
+                result += 4;
+                idx +=2;
+            } else
+            if (s.substring(idx).startsWith("IX")) {
+                result += 9;
+                idx +=2;
+            } else
             if (s.substring(idx).startsWith("M")) {
                 result += 1000;
                 idx +=1;
-            }
+            } else
             if (s.substring(idx).startsWith("D")) {
                 result += 500;
                 idx +=1;
-            }
+            } else
             if (s.substring(idx).startsWith("C")) {
-                result += 500;
+                result += 100;
+                idx +=1;
+            } else
+            if (s.substring(idx).startsWith("L")) {
+                result += 50;
+                idx +=1;
+            } else
+            if (s.substring(idx).startsWith("X")) {
+                result += 10;
+                idx +=1;
+            } else
+            if (s.substring(idx).startsWith("V")) {
+                result += 5;
+                idx +=1;
+            } else
+            if (s.substring(idx).startsWith("I")) {
+                result += 1;
                 idx +=1;
             }
         }
--- src/test/java/ru/code/workspace/problems/leetcode/tasks0/RomanInteger13Test.java
+++ src/test/java/ru/code/workspace/problems/leetcode/tasks0/RomanInteger13Test.java
@@ -1,4 +1,15 @@
 package ru.code.workspace.problems.leetcode.tasks0;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class RomanInteger13Test {
+    @Test
+    public void test_13() {
+        var decision = new RomanInteger13();
+        assertEquals(3, decision.romanToInt("III"));
+        assertEquals(58, decision.romanToInt("LVIII"));
+        assertEquals(1994, decision.romanToInt("MCMXCIV"));
+    }
 }
